3d787ff6f01d45576c01d45030fa10435c9f6c65
Mario DeSousa <mdesousa@metratech.com>
2012-10-15 13:05:27 -0400

    Moved connection properties to configuration file Improved enrollment example: - Extracted methods for logical operations like create an account or a credit card - Improved error handling by displaying message and error id in the console - Improved example by displaying invoice total amount and charge details

--- Metanga.Example/Program.cs
+++ Metanga.Example/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Globalization;
 using System.Net.Http;
 using Metanga.SoftwareDevelopmentKit.Proxy;
@@ -12,43 +13,122 @@ namespace Metanga.Example
   {
     static void Main(string[] args)
     {
-      // Example of an Enrollment to a Package that has a Reservation Product
-      // Please set the following values to appropriate values
-      var paymentBrokerAddress = "https://tenant.mypaymentaccess.com";
-      var address = new Uri("https://tenant.mybillaccess.com", UriKind.Absolute);
-      var username = "username";
-      var password = "password";
+      Console.WriteLine("Opening connection to Metanga...");
+      var client = OpenMetangaClient();
+      if (client == null)
+      {
+        EndExample();
+        return;
+      }
+
+      Console.WriteLine("Running Enrollment Example...");
+      EnrollmentExample(client);
+      Console.WriteLine("Closing connection to Metanga...");
+      CloseMetangaClient(client);
+      EndExample();
+    }
+
+    private static void EnrollmentExample(MetangaClient client)
+    {
       var externalAccountId = DateTime.Now.Ticks.ToString(CultureInfo.InvariantCulture);
       var externalSubscriptionId = DateTime.Now.Ticks.ToString(CultureInfo.InvariantCulture);
-      var creditCardNumber = "4111111111111111";
-      var creditCardType = "Visa";
-      var cardVerificationNumber = "123";
-      var cardExpiration = "12/2018";
+
+      var account = CreateAccount(externalAccountId);
+      var subscription = CreateSubscription(externalAccountId, externalSubscriptionId);
+      var creditCardToken = CreateCreditCardInPaymentBroker(account);
+
+      account.PaymentInstruments = new[] { new CreditCardMasked { InstrumentId = creditCardToken } }; // add the payment instrument id returned by payment broker to the account
+
+      bool enrollmentSucceeded;
+      Invoice invoice = null;
+      try
+      {
+        invoice = client.Enroll(subscription, account, InvoiceAction.InvoiceNow);
+        enrollmentSucceeded = true;
+      }
+      catch (MetangaException e)
+      {
+        Console.WriteLine("An error has occurred during enrollment: Id={0}, Message={1}", e.ErrorId, e.Message);
+        enrollmentSucceeded = false;
+      }
+
+      if (!enrollmentSucceeded) return;
+
+      Console.WriteLine("Account has been successfully enrolled. Account Id: {0}", externalAccountId);
+      if (invoice != null)
+      {
+        Console.WriteLine("An invoice has been created for {0} {1}", invoice.InvoiceCurrency,
+                          invoice.InvoiceSalesAmount + invoice.InvoiceTaxAmount);
+        foreach (var charge in invoice.Charges)
+        {
+          if (!charge.Product.EntityId.HasValue)
+            throw new InvalidOperationException("Product in Charge element has no EntityId"); // should not happen!
+          var product = client.RetrieveEntity<Product>(charge.Product.EntityId.Value);
+          Console.WriteLine("  - {0}: {1} {2}", product.Name["en-us"], invoice.InvoiceCurrency, charge.ChargeAmount);
+        }
+      }
+    }
+
+    private static void CloseMetangaClient(MetangaClient client)
+    {
+      try
+      {
+        client.Close();
+      }
+      catch (MetangaException e)
+      {
+        Console.WriteLine("An error has occurred while closing connection to Metanga: Id={0}, Message={1}", e.ErrorId, e.Message);
+      }
+    }
+
+    private static void EndExample()
+    {
+      Console.WriteLine("Press any key to finish");
+      Console.ReadKey();
+    }
+
+    private static MetangaClient OpenMetangaClient()
+    {
+      var metangaAddress = ConfigurationManager.AppSettings["MetangaAddress"];
+      var username = ConfigurationManager.AppSettings["MetangaUsername"];
+      var password = ConfigurationManager.AppSettings["MetangaPassword"];
+      var address = new Uri(metangaAddress, UriKind.Absolute);
+      MetangaClient client;
+      try
+      {
+        client = MetangaClient.Initialize(address, username, password, MetangaContentType.Json);
+      } catch (MetangaException e)
+      {
+        Console.WriteLine("An error has occurred while connecting to Metanga: Id={0}, Message={1}", e.ErrorId, e.Message);
+        return null;
+      }
+      return client;
+    }
+
+    private static Guid CreateCreditCardInPaymentBroker(Account account)
+    {
+      var paymentBrokerAddress = ConfigurationManager.AppSettings["PaymentBrokerAddress"];
+      const string creditCardNumber = "4111111111111111";
+      const string creditCardType = "Visa";
+      const string cardVerificationNumber = "123";
+      const string cardExpiration = "12/2018";
+
+      // Send request to payment broker to obtain a credit card token
+      return CreditCard.CreateCreditCard(paymentBrokerAddress, account.Address1, "", "",
+                                         cardVerificationNumber, account.City, account.Country,
+                                         creditCardNumber, creditCardType,
+                                         account.Email, cardExpiration, account.FirstName,
+                                         account.LastName, account.MiddleInitial, account.PhoneNumber,
+                                         account.Zip, account.State);
+    }
+
+    private static SampleSubscription CreateSubscription(string externalAccountId, string externalSubscriptionId)
+    {
       var packageId = new Guid("7923810c-8c96-4ff6-ac11-adae1303d8c3");
       var productId = new Guid("5ae6e359-2534-474f-8e48-1906f6f158fc");
-      var quantity = 5;
-      var unit = "MABIT";
+      const int quantity = 5;
+      const string unit = "MABIT";
       var enrollmentDate = new DateTime(2012, 8, 1);
-
-      var account = new SampleAccount
-                {
-                  ExternalId = externalAccountId,
-                  Name = new Dictionary<string, string> { { "en-us", externalAccountId }},
-                  FirstName = "John",
-                  MiddleInitial = "M",
-                  LastName = "Jones",
-                  Email = "john@mycompany.com",
-                  Address1 = "50 Main Street",
-                  City = "Waltham",
-                  State = "MA",
-                  Zip = "02451",
-                  Country = "US",
-                  PhoneNumber = "555-555-5555",
-                  BillingCycleUnit = "MO", // Monthly
-                  BillingCycleEndDate = new DateTime(2012, 1, 31), // Last Day of Month
-                  Currency = "USD",
-                  Payer = new Account { ExternalId = externalAccountId } // Self-Paid
-                };
 
       var subscriptionPackageProduct = new SubscriptionPackageProduct
                                          {
@@ -59,33 +139,39 @@ namespace Metanga.Example
                                          };
 
       var subscriptionPackageProducts = new [] {subscriptionPackageProduct};
-      
-      var subscription = new SampleSubscription
-                           {
-                             ExternalId = externalSubscriptionId,
-                             Account = new Account { ExternalId = externalAccountId },
-                             Package = new Package { EntityId = packageId },
-                             RecurringCycleUnitId = "MO",
-                             SubscriptionPackageProducts = subscriptionPackageProducts
-                           };
 
+      return new SampleSubscription
+               {
+                 ExternalId = externalSubscriptionId,
+                 Account = new Account { ExternalId = externalAccountId },
+                 Package = new Package { EntityId = packageId },
+                 RecurringCycleUnitId = "MO",
+                 SubscriptionPackageProducts = subscriptionPackageProducts
+               };
+    }
 
-      // Send request to payment broker to obtain a credit card token
-      var creditCardToken = CreditCard.CreateCreditCard(paymentBrokerAddress, account.Address1, "", "",
-                                                        cardVerificationNumber, account.City, account.Country,
-                                                        creditCardNumber, creditCardType,
-                                                        account.Email, cardExpiration, account.FirstName,
-                                                        account.LastName, account.MiddleInitial, account.PhoneNumber,
-                                                        account.Zip, account.State);
-
-      account.PaymentInstruments = new []{ new PaymentInstrumentMasked{InstrumentId = creditCardToken}}; // add the payment instrument id returned by payment broker to the account
-
-      var client = MetangaClient.Initialize(address, username, password, MetangaContentType.Json);
-      client.Enroll(subscription, account, InvoiceAction.InvoiceNext);
-      client.Close();
-
-      Console.WriteLine("Account has been successfully enrolled. Account Id: {0}", externalAccountId);
-      Console.ReadKey();
+    private static SampleAccount CreateAccount(string externalAccountId)
+    {
+      return new SampleAccount
+               {
+                 ExternalId = externalAccountId,
+                 Name = new Dictionary<string, string> { { "en-us", externalAccountId }},
+                 FirstName = "John",
+                 MiddleInitial = "M",
+                 LastName = "Jones",
+                 Email = "john@mycompany.com",
+                 Language = "en-us",
+                 Address1 = "50 Main Street",
+                 City = "Waltham",
+                 State = "MA",
+                 Zip = "02451",
+                 Country = "US",
+                 PhoneNumber = "555-555-5555",
+                 BillingCycleUnit = "MO", // Monthly
+                 BillingCycleEndDate = new DateTime(2012, 1, 31), // Last Day of Month
+                 Currency = "USD",
+                 Payer = new Account { ExternalId = externalAccountId } // Self-Paid
+               };
     }
   }
 
